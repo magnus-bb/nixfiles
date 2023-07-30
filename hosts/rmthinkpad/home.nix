76788c1caa5908b6a937657abfd6baaa37712f57
Magnus Bendix Borregaard <magnus.borregaard@gmail.com>
2023-07-30 22:27:34 +0200

    toggle ranger config for debug

--- hosts/rmthinkpad/home.nix
+++ hosts/rmthinkpad/home.nix
@@ -12,7 +12,7 @@
 	#* Or something like this perhaps
 	home.file = {
 		".config/ranger/rc.conf".source = ../../ranger/rc.conf; # ranger config
-		".config/ranger/plugins/ranger_devicons/__init__.py".source = ../ranger/plugins/ranger_devicons/__init__.py; # ranger icons plugin
+		".config/ranger/plugins/ranger_devicons/__init__.py".source = ../../ranger/plugins/ranger_devicons/__init__.py; # ranger icons plugin
 		".config/ranger/plugins/ranger_devicons/devicons.py".source = ../../ranger/plugins/ranger_devicons/devicons.py; # ranger icons plugin
 	};
 	
