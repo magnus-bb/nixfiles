2f8f6d97f3a98543b48c9f45feb9bd999dd6e8a1
magnus-bb <magnus.borregaard@gmail.com>
2023-08-05 22:08:06 +0200

    add hyprland test host to flake

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    inputs.hyprland.url = "github:hyprwm/Hyprland";
+    # inputs.hyprland.url = "github:hyprwm/Hyprland";
 
     spicetify-nix.url = "github:the-argus/spicetify-nix"; # theming for spotify
 
@@ -33,44 +33,83 @@
   outputs = { nixpkgs, nur, home-manager, ... }@inputs: 
   let
     system = "x86_64-linux";
-    host = "rmthinkpad";
+    # host = "rmthinkpad";
     user = "magnus";
   in {
     # Available through 'nixos-rebuild --flake .#host'
-    nixosConfigurations.${host} = nixpkgs.lib.nixosSystem {
-      inherit system;
+    nixosConfigurations = {
+      rmthinkpad = nixpkgs.lib.nixosSystem {
+        inherit system;
 
-      specialArgs = { inherit inputs host user; }; # Pass flake inputs to our config
+        specialArgs = { inherit inputs user; }; # Pass flake inputs to our config
 
-      modules = [
-        # This adds a nur configuration option.
-        # Use `config.nur` for packages like this:
-        # ({ config, ... }: {
-        #   environment.systemPackages = [ config.nur.repos.mic92.hello-nur ];
-        # })
-        nur.nixosModules.nur
+        modules = [
+          # This adds a nur configuration option.
+          # Use `config.nur` for packages like this:
+          # ({ config, ... }: {
+          #   environment.systemPackages = [ config.nur.repos.mic92.hello-nur ];
+          # })
+          nur.nixosModules.nur
 
-        ./hosts/${host}/configuration.nix # system wide configuration
+          ./hosts/rmthinkpad/configuration.nix # system wide configuration
 
-        ./hosts/${host}/hardware-configuration.nix # generated machine configuration ('nixos-generate-config')
+          ./hosts/rmthinkpad/hardware-configuration.nix # generated machine configuration ('nixos-generate-config')
 
-        home-manager.nixosModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.${user} = import ./hosts/${host}/home.nix;
-            # Use extraSpecialArgs to pass arguments to home.nix
-            extraSpecialArgs = { inherit inputs; };
-          };
+          home-manager.nixosModules.home-manager {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.${user} = import ./hosts/rmthinkpad/home.nix;
+              # Use extraSpecialArgs to pass arguments to home.nix
+              extraSpecialArgs = { inherit inputs; };
+            };
 
-          nixpkgs.overlays = [
-            # adds nur to pkgs: https://www.reddit.com/r/NixOS/comments/r9544v/comment/hnl6ywr/
-            # e.g. nur packages can be found with something like: pkgs.nur.repos.mic92.hello-nur
-            # modules from nur should be included in home.nix's 'imports [...]'
-            nur.overlay
-          ];
-        }
-      ];
+            nixpkgs.overlays = [
+              # adds nur to pkgs: https://www.reddit.com/r/NixOS/comments/r9544v/comment/hnl6ywr/
+              # e.g. nur packages can be found with something like: pkgs.nur.repos.mic92.hello-nur
+              # modules from nur should be included in home.nix's 'imports [...]'
+              nur.overlay
+            ];
+          }
+        ];
+      };
+
+      # Testing hyprland
+      rmthinkpad-hyprland = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        specialArgs = { inherit inputs user; }; # Pass flake inputs to our config
+
+        modules = [
+          # This adds a nur configuration option.
+          # Use `config.nur` for packages like this:
+          # ({ config, ... }: {
+          #   environment.systemPackages = [ config.nur.repos.mic92.hello-nur ];
+          # })
+          nur.nixosModules.nur
+
+          ./hosts/rmthinkpad-hyprland/configuration.nix # system wide configuration
+
+          ./hosts/rmthinkpad-hyprland/hardware-configuration.nix # generated machine configuration ('nixos-generate-config')
+
+          home-manager.nixosModules.home-manager {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.${user} = import ./hosts/rmthinkpad-hyprland/home.nix;
+              # Use extraSpecialArgs to pass arguments to home.nix
+              extraSpecialArgs = { inherit inputs; };
+            };
+
+            nixpkgs.overlays = [
+              # adds nur to pkgs: https://www.reddit.com/r/NixOS/comments/r9544v/comment/hnl6ywr/
+              # e.g. nur packages can be found with something like: pkgs.nur.repos.mic92.hello-nur
+              # modules from nur should be included in home.nix's 'imports [...]'
+              nur.overlay
+            ];
+          }
+        ];
+      };
     };
   };
 }
