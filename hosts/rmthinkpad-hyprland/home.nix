81012e48904970e87751197c66a15a5d63685e6f
magnus-bb <magnus.borregaard@gmail.com>
2023-08-10 21:56:54 +0200

    add swaylock theming and product sans font

--- hosts/rmthinkpad-hyprland/home.nix
+++ hosts/rmthinkpad-hyprland/home.nix
@@ -42,15 +42,14 @@ in {
 			source = ../../configs/swaylock-effects;
 		};
 
-		# # Product Sans font used by fufexan's eww bar
-  	# "${config.xdg.dataHome}/fonts/ProductSans".source = lib.cleanSourceWith {
-		# 	filter = name: _: (lib.hasSuffix ".ttf" (baseNameOf (toString name)));
-		# 	src = pkgs.fetchzip {
-		# 		url = "https://befonts.com/wp-content/uploads/2018/08/product-sans.zip";
-		# 		sha256 = "sha256-PF2n4d9+t1vscpCRWZ0CR3X0XBefzL9BAkLHoqWFZR4=";
-		# 		stripRoot = false;
-		# 	};
-		# };
+  	"${config.xdg.dataHome}/fonts/ProductSans".source = lib.cleanSourceWith {
+			filter = name: _: (lib.hasSuffix ".ttf" (baseNameOf (toString name)));
+			src = pkgs.fetchzip {
+				url = "https://befonts.com/wp-content/uploads/2018/08/product-sans.zip";
+				sha256 = "sha256-PF2n4d9+t1vscpCRWZ0CR3X0XBefzL9BAkLHoqWFZR4=";
+				stripRoot = false;
+			};
+		};
 
 		# ".config/eww" = {
 		# 	recursive = true;
