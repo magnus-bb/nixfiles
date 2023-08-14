679175812764592983eaf389e709177e1ad8495f
magnus-bb <magnus.borregaard@gmail.com>
2023-08-14 18:51:28 +0200

    finish rofi-wifi-menu

--- packages/rofi-wifi-menu/default.nix
+++ packages/rofi-wifi-menu/default.nix
@@ -1,33 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.stdenv.mkDerivation {
-	name = "rofi-wifi-menu";
+pkgs.writeShellApplication {
+  name = "rofi-wifi-menu";
 
-	nativeBuildInputs = [
-		pkgs.makeWrapper
-	];
+  runtimeInputs = [ pkgs.networkmanager ];
 
-  src = ./rofi-wifi-menu;
-
-  phases = [ "installPhase" "postFixup" ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src $out/bin
-  '';
-
-	# buildPhase = '''';
-
-	# installPhase = ''
-	# 	mkdir -p $out/bin
-	# 	cp -r rofi-wifi-menu $out/bin
-	# '';
-
-	# dontUnpack = true;
-
-	postBuild = ''
-		wrapProgram $out/bin/rofi-wifi-menu \
-			--set PATH ${pkgs.lib.makeBinPath (with pkgs; [
-				networkmanager
-			])}
-	'';
+	text = builtins.readFile ./rofi-wifi-menu;
 }
