65ae77a7948183a9f9495e01d2ad2ef420f30bb8
magnus-bb <magnus.borregaard@gmail.com>
2023-08-14 18:12:43 +0200

    change rofi-wifi-menu derivation

--- packages/rofi-wifi-menu/default.nix
+++ packages/rofi-wifi-menu/default.nix
@@ -2,7 +2,7 @@
 pkgs.stdenv.mkDerivation {
 	name = "rofi-wifi-menu";
 
-	src = "./.";
+	src = "./src";
 
 	nativeBuildInputs = [
 		pkgs.makeWrapper
@@ -10,7 +10,7 @@ pkgs.stdenv.mkDerivation {
 
 	installPhase = ''
 		mkdir -p $out/bin
-		cp -r $src/rofi-wifi-menu $out/bin
+		cp -r rofi-wifi-menu $out/bin
 	'';
 
 	postFixup = ''
